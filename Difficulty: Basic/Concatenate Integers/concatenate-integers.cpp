36dac83fcee85b3e38ce8a536fcd62e20b73a198
Divyanshu Choudhury <divyanshuchoudhury3@gmail.com>
2025-07-03 09:41:33 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Basic/Concatenate Integers/concatenate-integers.cpp
@@ -0,0 +1,10 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    int a, b;
+    cin >> a >> b;
+    // Write Your Code to Concate and Print
+    cout<<to_string(a)+to_string(b);
+    return 0;
+}
