7ee4535f3c532ef63b6ed832aec9a393b729427f
Divyanshu Choudhury <divyanshuchoudhury3@gmail.com>
2025-07-05 09:58:37 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Basic/Operations on Stack/operations-on-stack.cpp
@@ -0,0 +1,45 @@
+// User function Template for C++
+
+// Function to push an element into the stack.
+void insert(stack<int> &s, int x) {
+    // Your code here
+    s.push(x);
+}
+
+// Function to remove top element from stack.
+void remove(stack<int> &s) {
+    if(!s.empty()){
+         s.pop();  
+    }
+ 
+    // Your code here
+}
+
+// Function to print the top element of stack.
+void headOf_Stack(stack<int> &s) {
+    int x = s.top();// Your code here
+        cout << x << " " << endl;
+}
+
+// Function to search an element in the stack.
+bool find(stack<int> s, int val) {
+    bool exists = false;
+    
+
+
+    // Traverse the copy of the stack
+    while (!s.empty()) {
+        if (s.top() == val) {
+            exists = true;
+            break;
+        }
+        s.pop();
+    }
+    // Your code here
+
+    if (exists == true) {
+        return true;
+    } else {
+        return false;
+    }
+}
