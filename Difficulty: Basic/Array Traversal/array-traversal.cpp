5e7cccb4550dba9de9acde98111211771a2a0dcb
Divyanshu Choudhury <divyanshuchoudhury3@gmail.com>
2025-07-12 11:28:07 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Basic/Array Traversal/array-traversal.cpp
@@ -0,0 +1,9 @@
+void arrayTraversal(int numbers[], int size) {
+    // Code here
+    for(int i=0;i<size;i++){
+        cout<<numbers[i];
+           if (i != size - 1) {
+            cout << " ";
+           }
+    }
+}
