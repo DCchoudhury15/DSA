0701d3385566ff106c711682f4cb5e56316bf068
Divyanshu Choudhury <divyanshuchoudhury3@gmail.com>
2025-08-18 08:06:01 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Basic/Largest Element in Array/largest-element-in-array.cpp
@@ -0,0 +1,18 @@
+
+// User function Template for C++
+
+class Solution {
+  public:
+    int largest(vector<int> &arr) {
+        int max=arr[0];
+   for (int i=0;i<arr.size();i++){
+     if( arr[i]>max)
+          max=arr[i];
+          
+      
+}
+return max;// code here
+    }
+};
+
+
