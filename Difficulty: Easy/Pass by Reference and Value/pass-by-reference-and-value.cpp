2da6e7a15febf701c7c7c78ee81765603927f39c
Divyanshu Choudhury <divyanshuchoudhury3@gmail.com>
2025-08-20 20:05:23 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Easy/Pass by Reference and Value/pass-by-reference-and-value.cpp
@@ -0,0 +1,11 @@
+// User function Template for C++
+
+class Solution {
+  public:
+    vector<int> passedBy(int a, int &b) {
+        // code here
+        a=a+1;
+        b=b+2;
+        return {a,b};
+    }
+};
