92c6c9561a973ff12ce733d1b2fb0e066f1225b1
Divyanshu Choudhury <divyanshuchoudhury3@gmail.com>
2025-08-20 19:59:55 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Basic/Decision Making in Java/decision-making-in-java.cpp
@@ -0,0 +1,9 @@
+// User function Template for C++
+
+class Solution {
+  public:
+    string compareNM(int n, int m) {
+        // code here
+        return n == m?"equal":(n>m?"greater":"lesser");
+    }
+};
